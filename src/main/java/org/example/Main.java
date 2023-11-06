ffd6b5010b0f0b4c6db163b4946984a04a148c3b
Nurzhan <nurzhankydyralieva@gmail.com>
2023-11-06 17:17:00 +0600

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean testBean = context.getBean("testBean", TestBean.class);
-        System.out.println(testBean.getName());
+
+        MusicPlayer musicPlayer =context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
