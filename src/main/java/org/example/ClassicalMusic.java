08ab22bbea38840019c5d24938ba22eb8ea8bf7a
Nurzhan <nurzhankydyralieva@gmail.com>
2023-11-06 17:44:29 +0600

    third commit:

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,8 +1,24 @@
 package org.example;
 
-public class ClassicalMusic implements Music{
+public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void doInit() {
+        System.out.println("Doing initialization");
+    }
+
+    public void doDestroy() {
+        System.out.println("Doing destroy");
+    }
+
     @Override
     public String getSong() {
         return "Perfect Symphony";
     }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        MusicPlayer musicPlayer =context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//
+//        MusicPlayer musicPlayer =context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
         context.close();
     }
 }
