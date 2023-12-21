c6d0ff823628ea39d7837fdbe68ec3e47cce3c5c
Nurzhan <nurzhankydyralieva@gmail.com>
2023-12-21 19:05:19 +0600

    added sum double test

---
+++ src/main/java/org/codingBat/SumDouble.java
@@ -0,0 +1,14 @@
+package org.codingBat;
+
+public class SumDouble {
+    public static void main(String[] args) {
+
+    }
+    public int sumDouble(int a, int b) {
+        int sum = a + b;
+        if(a == b){
+            sum = sum * 2;
+        }
+        return sum;
+    }
+}
